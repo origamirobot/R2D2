b92ad8e28e506dd7c0e3fa295a28dc5bbcdfc6a4
Chris Lees <chris.t.lees@gmail.com>
2022-11-02 19:42:58 -0400

    Added logging

--- R2D2.Core/GPIO/DefaultGpioController.cs
+++ R2D2.Core/GPIO/DefaultGpioController.cs
@@ -1,4 +1,5 @@
-﻿using System.Device.Gpio;
+﻿using Microsoft.Extensions.Logging;
+using System.Device.Gpio;
 
 namespace R2D2.Core.GPIO
 {
@@ -13,7 +14,8 @@ namespace R2D2.Core.GPIO
 		#region PROTECTED PROPERTIES
 
 
-		protected GpioController _controller;
+		protected GpioController Controller { get; private set; }
+		protected ILogger<DefaultGpioController> Logger { get; private set; }
 
 
 		#endregion PROTECTED PROPERTIES
@@ -24,12 +26,12 @@ namespace R2D2.Core.GPIO
 		/// <summary>
 		/// Gets the numbering scheme.
 		/// </summary>
-		public PinNumberingScheme NumberingScheme => _controller.NumberingScheme;
+		public PinNumberingScheme NumberingScheme => Controller.NumberingScheme;
 
 		/// <summary>
 		/// Gets the number of pins this controller can control.
 		/// </summary>
-		public Int32 PinCount => _controller.PinCount;
+		public Int32 PinCount => Controller.PinCount;
 
 
 		#endregion PUBLIC ACCESSORS
@@ -41,16 +43,17 @@ namespace R2D2.Core.GPIO
 		/// Initializes a new instance of the <see cref="DefaultGpioController"/> class.
 		/// </summary>
 		/// <param name="numberingScheme">The numbering scheme.</param>
-		public DefaultGpioController(PinNumberingScheme numberingScheme = PinNumberingScheme.Board)
-			: this(new GpioController(numberingScheme)) { }
+		public DefaultGpioController(PinNumberingScheme numberingScheme, ILogger<DefaultGpioController> logger)
+			: this(new GpioController(numberingScheme), logger) { }
 
 		/// <summary>
 		/// Initializes a new instance of the <see cref="DefaultGpioController"/> class.
 		/// </summary>
 		/// <param name="controller">The controller.</param>
-		public DefaultGpioController(GpioController controller)
+		public DefaultGpioController(GpioController controller, ILogger<DefaultGpioController> logger)
 		{
-			_controller = controller;
+			Controller = controller;
+			Logger = logger;
 		}
 
 
@@ -64,13 +67,13 @@ namespace R2D2.Core.GPIO
 		/// </summary>
 		/// <param name="pinNumber">The pin number to close.</param>
 		public void ClosePin(Int32 pinNumber) 
-			=> _controller.ClosePin(pinNumber);
+			=> Controller.ClosePin(pinNumber);
 
 		/// <summary>
 		/// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
 		/// </summary>
 		public void Dispose()
-			=> _controller.Dispose();
+			=> Controller.Dispose();
 
 		/// <summary>
 		/// Gets the mode for an already open pin.
@@ -78,7 +81,7 @@ namespace R2D2.Core.GPIO
 		/// <param name="pinNumber">The number of the pin to get the mode for.</param>
 		/// <returns></returns>
 		public PinMode GetPinMode(Int32 pinNumber)
-			=> _controller.GetPinMode(pinNumber);
+			=> Controller.GetPinMode(pinNumber);
 
 		/// <summary>
 		/// Determines whether the pin mode is supported on the specified pin.
@@ -87,7 +90,7 @@ namespace R2D2.Core.GPIO
 		/// <param name="mode">The mode to check.</param>
 		/// <returns></returns>
 		public bool IsPinModeSupported(Int32 pinNumber, PinMode mode)
-			=> _controller.IsPinModeSupported(pinNumber, mode);
+			=> Controller.IsPinModeSupported(pinNumber, mode);
 
 		/// <summary>
 		/// Determines whether the specified pin is already open.
@@ -95,14 +98,14 @@ namespace R2D2.Core.GPIO
 		/// <param name="pinNumber">The number of the pin to check.</param>
 		/// <returns></returns>
 		public bool IsPinOpen(Int32 pinNumber)
-			=> _controller.IsPinOpen(pinNumber);
+			=> Controller.IsPinOpen(pinNumber);
 
 		/// <summary>
 		/// Opens the specified pin for use.
 		/// </summary>
 		/// <param name="pinNumber">The pin number.</param>
 		public void OpenPin(Int32 pinNumber)
-			=> _controller.OpenPin(pinNumber);
+			=> Controller.OpenPin(pinNumber);
 
 		/// <summary>
 		/// Opens the specified pin using the provided <see cref="PinMode" />
@@ -110,37 +113,37 @@ namespace R2D2.Core.GPIO
 		/// <param name="pinNumber">The pin number to set.</param>
 		/// <param name="mode">The mode to set for this pin after it is opened.</param>
 		public void OpenPin(Int32 pinNumber, PinMode mode)
-			=> _controller.OpenPin(pinNumber, mode);
+			=> Controller.OpenPin(pinNumber, mode);
 
 		/// <summary>
 		/// Reads the value from the specified pin.
 		/// </summary>
 		/// <param name="pinNumber">The pin number to read from.</param>
 		/// <returns></returns>
-		public PinValue Read(int pinNumber)
-			=> _controller.Read(pinNumber);
+		public PinValue Read(Int32 pinNumber)
+			=> Controller.Read(pinNumber);
 
 		/// <summary>
 		/// Reads all of the values from the specified pins
 		/// </summary>
 		/// <param name="valuePairs">The value pairs.</param>
 		public void Read(Span<PinValuePair> valuePairs)
-			=> _controller.Read(valuePairs);
+			=> Controller.Read(valuePairs);
 
 		/// <summary>
 		/// Writes the value to the specified pin.
 		/// </summary>
 		/// <param name="pinNumber">The pin to write to.</param>
 		/// <param name="value">The value to write.</param>
-		public void Write(int pinNumber, PinValue value)
-			=> _controller.Write(pinNumber, value);
+		public void Write(Int32 pinNumber, PinValue value)
+			=> Controller.Write(pinNumber, value);
 
 		/// <summary>
 		/// Writes the values to the specified pins.
 		/// </summary>
 		/// <param name="pinValues">The pins.</param>
 		public void Write(ReadOnlySpan<PinValuePair> pinValues)
-			=> _controller.Write(pinValues);
+			=> Controller.Write(pinValues);
 
 		/// <summary>
 		/// Takes an already open pin and sets it's mode.
@@ -148,7 +151,7 @@ namespace R2D2.Core.GPIO
 		/// <param name="pinNumber">The number of the pin to set.</param>
 		/// <param name="mode">The mode to set for this open pin.</param>
 		public void SetPinMode(Int32 pinNumber, PinMode mode)
-			=> _controller.SetPinMode(pinNumber, mode);
+			=> Controller.SetPinMode(pinNumber, mode);
 
 
 		#endregion PUBLIC METHODS
@@ -163,7 +166,7 @@ namespace R2D2.Core.GPIO
 		/// <param name="eventTypes">The event types.</param>
 		/// <param name="callback">The callback.</param>
 		public void RegisterCallbackForPinValueChangedEvent(int pinNumber, PinEventTypes eventTypes, PinChangeEventHandler callback)
-			=> _controller.RegisterCallbackForPinValueChangedEvent(pinNumber, eventTypes, callback);
+			=> Controller.RegisterCallbackForPinValueChangedEvent(pinNumber, eventTypes, callback);
 
 		/// <summary>
 		/// Unregisters the callback for pin value changed event.
@@ -171,7 +174,7 @@ namespace R2D2.Core.GPIO
 		/// <param name="pinNumber">The pin number.</param>
 		/// <param name="callback">The callback.</param>
 		public void UnregisterCallbackForPinValueChangedEvent(int pinNumber, PinChangeEventHandler callback)
-			=> _controller.UnregisterCallbackForPinValueChangedEvent(pinNumber, callback);
+			=> Controller.UnregisterCallbackForPinValueChangedEvent(pinNumber, callback);
 
 		/// <summary>
 		/// Waits for the specified event to occur.
@@ -181,7 +184,7 @@ namespace R2D2.Core.GPIO
 		/// <param name="timeout">The amount of time to wait before timing out.</param>
 		/// <returns></returns>
 		public WaitForEventResult WaitForEvent(int pinNumber, PinEventTypes eventTypes, TimeSpan timeout)
-			=> _controller.WaitForEvent(pinNumber, eventTypes, timeout);
+			=> Controller.WaitForEvent(pinNumber, eventTypes, timeout);
 
 		/// <summary>
 		/// Waits for the specified event to occur.
@@ -191,7 +194,7 @@ namespace R2D2.Core.GPIO
 		/// <param name="cancellationToken">Cancellation token.</param>
 		/// <returns></returns>
 		public WaitForEventResult WaitForEvent(int pinNumber, PinEventTypes eventTypes, CancellationToken cancellationToken)
-			=> _controller.WaitForEvent(pinNumber, eventTypes, cancellationToken);
+			=> Controller.WaitForEvent(pinNumber, eventTypes, cancellationToken);
 
 		/// <summary>
 		/// Waits for the specified event to occur.
@@ -201,7 +204,7 @@ namespace R2D2.Core.GPIO
 		/// <param name="timeout">The amount of time to wait before timing out.</param>
 		/// <returns></returns>
 		public async ValueTask<WaitForEventResult> WaitForEventAsync(int pinNumber, PinEventTypes eventTypes, TimeSpan timeout)
-			=> await _controller.WaitForEventAsync(pinNumber, eventTypes, timeout);
+			=> await Controller.WaitForEventAsync(pinNumber, eventTypes, timeout);
 
 		/// <summary>
 		/// Waits for the specified event to occur.
@@ -211,7 +214,7 @@ namespace R2D2.Core.GPIO
 		/// <param name="cancellationToken">Cancellation token.</param>
 		/// <returns></returns>
 		public async ValueTask<WaitForEventResult> WaitForEventAsync(int pinNumber, PinEventTypes eventTypes, CancellationToken cancellationToken)
-			=> await _controller.WaitForEventAsync(pinNumber, eventTypes, cancellationToken);
+			=> await Controller.WaitForEventAsync(pinNumber, eventTypes, cancellationToken);
 
 
 		#endregion EVENT METHODS
